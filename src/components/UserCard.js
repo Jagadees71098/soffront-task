330370b7cf72da7b3baa1dd1cc4c8e2acf27509f
Jagadees71098 <jagadeeswaran71098@gmail.com>
2024-08-31 19:57:08 +0530

    Update UserCard.js

--- src/components/UserCard.js
+++ src/components/UserCard.js
@@ -24,7 +24,7 @@ function UserCard({ user }) {
                     id="panel1-header"
                 >
                     <Stack direction={{ xs: 'column', sm: 'row' }} width={'100%'} justifyContent={'space-between'} alignItems={{ xs: 'normal', sm: 'end' }} spacing={2}>
-                        <Stack direction="row" spacing={2}>
+                        <Stack direction={{ xs: 'column', md: 'row' }} spacing={2}>
                             <Avatar
                                 alt={`${user.login}'s avatar`}
                                 src={user.avatar_url}
@@ -32,7 +32,7 @@ function UserCard({ user }) {
                             />
                             <Stack spacing={-0.5}>
                                 <Typography variant="h6">{user.login}</Typography>
-                                <Stack direction="row" spacing={0.5}>
+                                <Stack direction={{ xs: 'column', md: 'row' }} spacing={0.5}>
                                     <Typography>Profile URL:</Typography>
                                     <Link href={user.html_url} target="_blank" >{user.html_url}</Link>
                                 </Stack>
